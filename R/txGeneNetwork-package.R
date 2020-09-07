9ef7ff76d355b8633272639ac8f57b41c63bca53
luciorq <luciorqueiroz@gmail.com>
2020-09-07 17:36:51 -0300

    added pipe and tibble support

---
+++ R/txGeneNetwork-package.R
@@ -0,0 +1,7 @@
+## usethis namespace: start
+#' @importFrom tibble tibble
+## usethis namespace: end
+NULL
+
+# use this when a returned object is a tibble
+# @return a [tibble][tibble::tibble-package]
